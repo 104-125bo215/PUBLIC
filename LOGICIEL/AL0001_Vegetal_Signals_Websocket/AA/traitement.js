0017500d1af366a693d716e85aa404166f2b43ef
104-125bo215 <maxime.bezanilla@gadz.org>
2018-03-17 00:23:22 +0100

    Add files via upload

---
+++ LOGICIEL/AL0001_Vegetal_Signals_Websocket/AA/traitement.js
@@ -0,0 +1,29 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getDataFromWS() {
+    exampleSocket.onmessage = function (event) {
+        return event.data;
+    }
+};
+
+function fake_getDataFromWS() {
+    var trace1;
+    var trace2;
+    var data;
+
+    trace1 = {
+        x: [1, 2, 3, 4],
+        y: [10, getRandomInt(20), 13, 17],
+        mode: 'lines'
+    };
+
+    trace2 = {
+        x: [2, 3, 4, 5],
+        y: [16, 5, 11, 10],
+        mode: 'lines'
+    };
+    data = [trace1, trace2];
+    return data;
+};
